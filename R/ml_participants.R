a3fdb4d3e7d4f45337eeb430cdd21912e6fa942e
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-11-17 11:42:57 +0100

    Rename functions

---
+++ R/ml_participants.R
@@ -0,0 +1,31 @@
+#### ml_participants: Update participant database ##############################
+
+#' Retrieve and update local and/or remote data from formr
+#' @param google_email E-mail used in Google Drive account.
+#' @return A data frame with all participants that have participated or are candidates to participate in any of the versions of MultiLex
+#' @examples
+#' participants <- ml_participants(email = "user@mail.com")
+#'
+
+
+ml_participants <- function(
+  google_email = NULL
+) {
+  suppressMessages(
+    {
+      googlesheets4::gs4_auth(email = google_email)
+      participants <- googlesheets4::read_sheet("164DMKLRO0Xju0gdfkCS3evAq9ihTgEgFiuJopmqt7mo",
+                                                sheet = "Participants") %>%
+        drop_na(code) %>%
+        select(-version)
+      googlesheets4::gs4_deauth()
+    }
+  )
+  # make sure no columns are lists (probably due to inconsistent cell types)
+  if (any(purrr::map_lgl(participants, is.list))) {
+    col <- names(which(purrr::map_lgl(participants, is.list)))
+    stop(paste0("Column(s) ", col, " is a list. Check that all its values are of the same format."))
+  }
+  return(participants)
+}
+
