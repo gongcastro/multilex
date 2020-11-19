d7ff2e7b2a19be179d489bbf446b1429a6aa1232
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-11-19 16:43:14 +0100

    Update documentation and add tests (issue #6)

---
+++ tests/testthat/test-workflow.R
@@ -0,0 +1,27 @@
+participants <- ml_participants("gonzalo.garciadecastro@upf.edu")
+
+test_that("ml_participants columns are the right classes", {
+  expect_true(is.character(participants$id))
+  expect_true(is.character(participants$id_exp))
+  expect_true(is.character(participants$id_db))
+  expect_true(is.character(participants$code))
+  expect_true(is.numeric(participants$time))
+  expect_true(lubridate::is.POSIXct(participants$date_birth))
+  expect_true(is.numeric(participants$age_now))
+  expect_true(is.character(participants$study))
+  expect_true(lubridate::is.POSIXct(participants$date_test))
+  expect_true(is.character(participants$language))
+  expect_true(is.character(participants$call))
+  expect_true(is.logical(participants$email_ready))
+  expect_true(is.logical(participants$email_sent))
+  expect_true(is.logical(participants$reminded))
+  expect_true(is.logical(participants$completed))
+  expect_true(is.character(participants$cdi))
+  expect_true(lubridate::is.POSIXct(participants$date_sent))
+  expect_true(is.character(participants$first_contact))
+  expect_true(is.character(participants$comments))
+})
+
+test_that("participants and times are not duplicated", {
+  expect_false(any(duplicated(select(participants, id, time))))
+})
