7519a5749769b846b40545b2590c11e2852c4bdb
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-11-20 16:38:23 +0100

    Fix bug in fix_doe function

--- R/utils.R
+++ R/utils.R
@@ -30,10 +30,11 @@ fix_doe <- function(x) {
            id_db=="54469" & time==2 ~ 0,
            id_db=="57157" & time==1 ~ 80,
            id_db=="57046" & time==1 ~ 50,
-           TRUE ~ as.numeric(doe_catalan)
+           TRUE ~ doe_catalan
          ),
          doe_spanish = case_when(
-           id_db=="57046" & time==1 ~ 50
+           id_db=="57046" & time==1 ~ 50,
+           TRUE ~ doe_spanish
          )
   )
 }
