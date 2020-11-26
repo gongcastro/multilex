e63c3e8819216bc1ba0091721f1ef533b63cffde
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-11-26 15:07:17 +0100

    Add bilexicon_1124 to fix_sex

--- R/utils.R
+++ R/utils.R
@@ -51,7 +51,10 @@ fix_doe <- function(x) {
 fix_sex <- function(x) {
   x %>%
     group_by(id) %>%
-    mutate(sex = case_when(id %in% c("bilexicon_1097", "bilexicon_1441", "bilexicon_1448") ~ "Female",
+    mutate(sex = case_when(id %in% c("bilexicon_1097",
+                                     "bilexicon_1441",
+                                     "bilexicon_1124",
+                                     "bilexicon_1448") ~ "Female",
                            id %in% c("bilexicon_1447") ~ "Male",
                            TRUE ~ sex[which(!is.na(sex))[1]])) %>%
     ungroup()
